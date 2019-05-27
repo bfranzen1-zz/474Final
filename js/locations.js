9dc539d9617287940383c00087e12793d0ec1097
bfranzen1 <bef6823@gmail.com>
2019-05-26 19:09:01 -0700

    fix data

---
+++ js/locations.js
@@ -0,0 +1,14 @@
+var locations = {
+    'North Station': '42.3661, -71.0631', 
+    'Northeastern University': '42.3398, -71.0892', 
+    'West End': '42.364400, -71.066100',
+    'Haymarket Square': '42.363800, -71.058500', 
+    'South Station': '42.351900, -71.055100',
+    'Fenway': '42.342900, -71.100300',
+    'Theatre District': '42.351900, -71.064300',
+    'Beacon Hill': '42.358800, -71.070700',
+    'Back Bay': '42.350300, -71.081000',
+    'North End': '42.364700, -71.054200',
+    'Financial District': '42.355900, -71.055000',
+    'Boston University': '42.350500, -71.105400' 
+}
